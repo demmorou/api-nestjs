f97c1ddb9cb1d1fdb277c39438fa5b8da1cdbcd0
Deusimar <deusimar@ufpi.edu.br>
2020-03-24 19:43:19 -0300

    feat: ok

--- src/main.ts
+++ src/main.ts
@@ -4,5 +4,6 @@ import { AppModule } from './modules/app/app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
+  console.log('ok');
 }
 bootstrap();
